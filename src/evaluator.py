80135c8ca1eb3b09736e91157a873a0144157c74
AleksaAleksZakharov <AleksaAleksZakharov@Greenatom.ru>
2026-05-16 03:42:50 +0300

    3

--- src/evaluator.py
+++ src/evaluator.py
@@ -109,8 +109,8 @@ score = (подтверждённые / всего утверждений)
 
         try:
             response = self.llm.invoke(prompt)
-            raise Exception(f'{response}')
-            print(response)
+
+            print(f'RAGEvaluator response LLM {response}')
             # Извлекаем JSON из ответа (на случай, если модель добавила текст)
             json_start = response.find('{')
             json_end = response.rfind('}') + 1
